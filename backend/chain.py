fb65f3a2ad73db5132dfdfeca2e9fcecbe7ae0ce
231B417 <231B417@juetguna.in>
2026-05-29 13:28:30 +0530

    Langchain changes

--- backend/chain.py
+++ backend/chain.py
@@ -1,5 +1,5 @@
 from langchain_groq import ChatGroq
-from langchain.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate
 from dotenv import load_dotenv
 import os
 import json
